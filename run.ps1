2f721a49c403af385f05161c05bbbe41fe23fe60
IgnacioBona <ignacio.bona-ribot@epitech.eu>
2025-05-17 17:19:07 +0800

    Revert "[feat] [#4] Added whisper.cpp to POC"
    This reverts commit 30bf19d8e1315ea921cb1461463287d3055afefc.

--- run.ps1
+++ run.ps1
@@ -1,58 +1,31 @@
-# # run.ps1
 
-# # ----------------------------
-# # 1) Clone or Update PortAudio
-# # ----------------------------
-# if (Test-Path -Path "./external/portaudio") {
-#     Write-Host "Updating portaudio (git pull)..."
-#     Set-Location -Path "./external/portaudio"
-#     git pull
-#     Set-Location -Path "../.."
-# } else {
-#     Write-Host "Cloning portaudio..."
-#     git clone https://github.com/PortAudio/portaudio.git external/portaudio
-# }
-
-# # --------------------------------
-# # 2) Clone or Update whisper.cpp
-# # --------------------------------
-# if (Test-Path -Path "./external/whisper.cpp") {
-#     Write-Host "Updating whisper.cpp (git pull)..."
-#     Set-Location -Path "./external/whisper.cpp"
-#     git pull
-#     Set-Location -Path "../.."
-# } else {
-#     Write-Host "Cloning whisper.cpp..."
-#     git clone https://github.com/ggerganov/whisper.cpp.git external/whisper.cpp
-# }
-
-# ----------------------------------
-# 3) Create / Enter Build Directory
-# ----------------------------------
-if (-not (Test-Path -Path "./build")) {
-    New-Item -ItemType Directory -Path "./build" | Out-Null
+# Check if there is a external folder
+if (Test-Path -Path "./external/portaudio") {
+    # Update the portaudio library
+    Set-Location -Path "./external/portaudio"
+    git pull
+    Set-Location -Path "../.."
+} else {
+    # Clone the portaudio library
+    git clone https://github.com/PortAudio/portaudio.git external/portaudio
 }
 
+# Check if there is a bulid folder
+if (-not (Test-Path -Path "./build")) {
+    New-Item -ItemType Directory -Path "./build"
+}
+
+# Navigate to the build directory
 Set-Location -Path "./build"
 
-# --------------------------------
-# 4) Generate the Build Files
-#    - Force BUILD_SHARED_LIBS=ON
-#    - Choose Release configuration
-# --------------------------------
-Write-Host "Configuring CMake..."
-cmake -D BUILD_SHARED_LIBS=ON -D CMAKE_BUILD_TYPE=Release ..
+# Generate the build files
+cmake ..
 
-# -------------------------------------------------
-# 5) Build the Project (Release mode on Windows)
-# -------------------------------------------------
-Write-Host "Building project..."
+# Compile the project
 cmake --build . --config Release
 
-# ------------------------------------------
-# 6) Return to root and run the .exe
-# ------------------------------------------
+# Back to the root directory
 Set-Location -Path ".."
 
-# Write-Host "Running executable..."
+# Run the resulting executable
 ./build/Release/AudioCapturePlayback.exe
